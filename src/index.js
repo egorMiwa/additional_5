4f5174d841d42fbefe7c82c0c4bdfb64fee43780
egorMiwa <36693566+egorMiwa@users.noreply.github.com>
2018-02-26 19:48:29 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  var brackets = String(bracketsConfig).split(',');
+  var order = [];
+  var last;
+
+  if( str.length % 2)
+  {
+    return(false);
+        }
+
+  for(var i = 0; i<str.length; i++)
+  {
+    for(var k = 0; k<brackets.length; k = k+2)
+    {
+        if(str[i] == brackets[k+1] && last == brackets[k])
+        {
+            order.pop();
+            last = order[order.length-1];
+        }
+        else if(str[i] == brackets[k])
+        {
+            order.push(brackets[k])
+            last = order[order.length-1];
+            break;
+        }
+    }
+  }
+return(order.length==0);
 }
